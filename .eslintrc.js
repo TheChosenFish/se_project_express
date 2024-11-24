4200598cfb7297ef29b102e8c6132ba195fb6256
Michael B <mbostick94@yahoo.com>
2024-11-24 14:51:56 -0500

    new repository

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+console.error("asdf");
+
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "airbnb-base", "prettier"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "no-underscore-dangle": ["warn", { allow: ["error"] }],
+    "no-console": ["warn", { allow: ["error"] }],
+  },
+};
