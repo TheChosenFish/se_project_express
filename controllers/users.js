26bcf1256b7dca048b25444ed35bd6b0f9082f27
Michael B <mbostick94@yahoo.com>
2025-01-05 15:43:15 -0500

    add controller changes

--- controllers/users.js
+++ controllers/users.js
@@ -63,10 +63,10 @@ const updateUser = (req, res) => {
     .then((user) => res.status(200).send(user))
     .catch((err) => {
       if (err.name === 'ValidationError' ) {
-        return res.status(NOT_FOUND).send({ message: "User not found" })
+        return res.status(BAD_REQUEST).send({ message: "User not found" })
   }
       if (err.name === "DocumentNotFoundError") {
-        return res.status(BAD_REQUEST).send({ message: "Bad request" });
+        return res.status(NOT_FOUND).send({ message: "Bad request" });
       }
       return res.status(DEFAULT).send({ err: err.message });
     });
